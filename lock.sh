1d289d6ac7806ef65f714a5d8dd956f712e3135d
mevouc <meven.courouble@vouc.me>
2015-10-17 10:53:02 +0200

    rm screenshot picture after lock.

--- lock.sh
+++ lock.sh
@@ -3,3 +3,4 @@ scrot ~/lock.png
 convert -scale 10% -scale 1000% ~/lock.png -modulate 100,0 ~/configs/lockscreen/picture
 epi3lock -e -i ~/configs/lockscreen/picture -f -n
 rm ~/configs/lockscreen/picture
+rm ~/lock.png

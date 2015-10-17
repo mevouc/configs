273e96d03be7371281a2527f6eefa5da0ce86564
mevouc <meven.courouble@vouc.me>
2015-10-17 03:10:31 +0200

    grayscale on lockscreen.

--- lock.sh
+++ lock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 scrot ~/lock.png
-convert -scale 10% -scale 1000% ~/lock.png ~/configs/lockscreen/picture
+convert -scale 10% -scale 1000% ~/lock.png -modulate 100,0 ~/configs/lockscreen/picture
 epi3lock -e -i ~/configs/lockscreen/picture -f -n
 rm ~/configs/lockscreen/picture

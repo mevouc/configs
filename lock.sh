38bcb40f9f8c6af1e7790a4dbc96720139b37adf
mevouc <meven.courouble@vouc.me>
2015-10-18 02:07:21 +0200

    Test for black & white pixelited lockscreen.

--- lock.sh
+++ lock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 scrot ~/lock.png
-convert -scale 10% -scale 1000% ~/lock.png -modulate 100,0 ~/configs/lockscreen/picture
+convert -scale 10% -monochrome -scale 1000% ~/lock.png '#-modulate 100,150' ~/configs/lockscreen/picture
 epi3lock -e -i ~/configs/lockscreen/picture -f -n
 rm ~/configs/lockscreen/picture
 rm ~/lock.png

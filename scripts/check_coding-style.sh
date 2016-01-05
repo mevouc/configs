ea9ab0bfbc7001e04042697310c37d66c325fa1d
mevouc <meven.courouble@vouc.me>
2016-01-05 00:24:45 +0000

    More cases in coding style checker

--- scripts/check_coding-style.sh
+++ scripts/check_coding-style.sh
@@ -9,6 +9,27 @@ echo
 echo "> Lines containing tabs:"
 grep -n --color '	' $1
 echo
+echo "> Missing spaces after keyword with arguments:"
+grep -n --color 'if(' $1
+grep -n --color 'while(' $1
+grep -n --color 'for(' $1
+grep -n --color 'switch(' $1
+grep -n --color 'sizeof(' $1
+echo
+echo "> Space before semicolon:"
+grep -n --color ' ;' $1
+echo
+echo "> Spaces after openning parentheses:"
+grep -n --color '( ' $1
+echo
+echo "> Spaces before closing parentheses:"
+grep -n --color ' )' $1
+echo
+echo "> Padding errors with expressions:"
+grep -n --color ' :' $1 # Switch case
+# TODO spaces before and after +, -, *, /, =, +=, ...
+echo
 echo "Done."
 
 # END
+
